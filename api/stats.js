3cd7d2ef03a34e93185a7faca2486410a5fe2c4d
LinuxFinn <judahwardlaw407@protonmail.com>
2026-07-27 15:03:22 -0400

    Refactor config to export runtime directly

--- api/stats.js
+++ api/stats.js
@@ -1,8 +1,6 @@
 import { ImageResponse } from '@vercel/og';
 
-export const config = {
-  runtime: 'edge',
-};
+export const runtime = 'edge';
 
 async function fetchAniListStats(username) {
   const query = `
